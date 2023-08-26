aeb70659483565cdb208aeeaccc031b43a8de37f
Aatir1224 <aatirqureshi1224@gmail.com>
2023-08-26 17:12:19 +0530

    deployment changes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.use(express.static(__dirname + "/dist/csm-frontend"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/dist/csm-frontend/index.html");
+});
+
+app.listen(PORT);
